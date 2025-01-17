fb1cba2aec5f73ad628004a6a2938e550f01d1a5
Thy Muoyhak <thymuoyhak.biu@gmail.com>
2025-01-17 13:25:11 +0700

    Update project for new deployment

--- src/App.js
+++ src/App.js
@@ -1,42 +1,32 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/constaint/Header";
 import Footer from "./components/constaint/Footer";
 import Home from "./page/Home";
-
 import About from "./page/About";
 import Contact_page from "./page/Contact_page";
 import C_programming from "./components/course/C_Programming";
 import Cpp_programming from "./components/course/Cpp_Programming";
+
 function App() {
   return (
     <div>
       <section>
-        <Header></Header>
+        <Header />
       </section>
 
-      <BrowserRouter>
+      <Router>
         <Routes>
-          <Route path="/home" element={<Home></Home>}></Route>
-          <Route index element={<Home></Home>}></Route>
-          <Route path="/about" element={<About></About>}></Route>
-          <Route
-            path="/contact"
-            element={<Contact_page></Contact_page>}
-          ></Route>
-          <Route
-            path="c_programming"
-            element={<C_programming></C_programming>}
-          ></Route>
-          <Route
-            path="cpp_programming"
-            element={<Cpp_programming></Cpp_programming>}
-          ></Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact_page />} />
+          <Route path="/c_programming" element={<C_programming />} />
+          <Route path="/cpp_programming" element={<Cpp_programming />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
 
       <section>
-        <Footer></Footer>
+        <Footer />
       </section>
     </div>
   );
